f94b85da2ed1308e31612a13245891386ec09739
kell888 <421979530@qq.com>
2016-06-03 17:13:20 +0800

    2016-06-03

---
+++ 受控端/MonitorServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.IO;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace 远程监控
+{
+    class MonitorServer
+    {
+        static void Main(string[] args)
+        {
+            TcpServerChannel channel = new TcpServerChannel(9000);
+            ChannelServices.RegisterChannel(channel,false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Monitor), "MonitorServerUrl", WellKnownObjectMode.SingleCall);
+            Console.ReadLine(); 
+        }
+    }
+}
